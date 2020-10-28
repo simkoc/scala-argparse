07269a9b3f0bf6386b6a19b0976e891fddb48047
Simon Koch <simon.koch@tu-braunschweig.de>
2020-10-28 17:31:51 +0100

    added two exception classes I forgot to add previously

---
+++ src/main/scala/de/halcony/argparse/ParsingException.scala
@@ -0,0 +1,13 @@
+package de.halcony.argparse
+
+class ParsingException(message : String) extends Exception {
+
+  override def getMessage: String = message
+
+}
+
+private[argparse] class IntermediateParsingException(message : String) extends  Exception {
+
+  override def getMessage: String = message
+
+}
