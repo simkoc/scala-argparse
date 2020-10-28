26e6e6e3a29ff59fb1d6e24d58c9c9feeb843940
Simon Koch <simon.koch@tu-braunschweig.de>
2020-10-28 17:31:29 +0100

    scalafmt

--- src/main/scala/de/halcony/argparse/ComandLineParser.scala
+++ src/main/scala/de/halcony/argparse/ComandLineParser.scala
@@ -4,7 +4,7 @@ import scala.collection.mutable.ListBuffer
 import scala.util.control.Breaks.{break, breakable}
 
 
-private abstract class CommandLineParser(val name : String,val description : String) {
+abstract class CommandLineParser(val name : String,val description : String) {
 
   private[argparse] var parsed : Boolean = false
 
@@ -14,8 +14,7 @@ private abstract class CommandLineParser(val name : String,val description : Str
 
 }
 
-
-private case class Positional(override val name : String, override val description : String = "") extends CommandLineParser(name, description) {
+case class Positional(override val name : String, override val description : String = "") extends CommandLineParser(name, description) {
 
   var value : String = ""
 
@@ -30,7 +29,7 @@ private case class Positional(override val name : String, override val descripti
   }
 }
 
-private case class Optional(override val name : String, short : String, long : String = "", default : Option[String] = None, override val description : String = "") extends CommandLineParser(name, description) {
+case class Optional(override val name : String, short : String, long : String = "", default : Option[String] = None, override val description : String = "") extends CommandLineParser(name, description) {
 
   var value : Option[String] = default
 
@@ -68,7 +67,7 @@ private case class Optional(override val name : String, short : String, long : S
   }
 }
 
-private case class Flag(override val name : String, short : String, long : String = "", override val description : String = "") extends CommandLineParser(name, description) {
+case class Flag(override val name : String, short : String, long : String = "", override val description : String = "") extends CommandLineParser(name, description) {
 
   var value : Boolean = false
 
@@ -100,7 +99,7 @@ private case class Flag(override val name : String, short : String, long : Strin
   }
 }
 
-private case class Parser(override val name : String, override val description : String = "") extends CommandLineParser(name, description) {
+case class Parser(override val name : String, override val description : String = "") extends CommandLineParser(name, description) {
 
   private val positionals : collection.mutable.ListBuffer[CommandLineParser] = new ListBuffer[CommandLineParser]
   private val optionals : collection.mutable.ListBuffer[CommandLineParser] = new ListBuffer[CommandLineParser]
@@ -128,6 +127,15 @@ private case class Parser(override val name : String, override val description :
     this
   }
 
+  def chosenSubparser() : (String,Parser) = {
+    if(subparsers.isEmpty) {
+      throw new RuntimeException("the chosen parser has no subparser")
+    }
+    subparsers.filter(_.parsed).head match {
+      case x : Parser => (x.name, x)
+    }
+  }
+
   def get[T](name : String) : T = {
     var ret : Option[T] = None
     breakable {
@@ -145,47 +153,61 @@ private case class Parser(override val name : String, override val description :
     ret.getOrElse(throw new RuntimeException(s"there is no parser of name $name"))
   }
 
-  override def parse(args: List[String]): List[String] = {
+  private def parsePositionals(args: List[String]): List[String] = {
     var sargs = args
-    var remaining : List[String] = Nil
-    if(args.nonEmpty && (parentParsers.isEmpty || args.head == name)) {
-      parsed = true
-      if (parentParsers.nonEmpty) {
-        sargs = args.tail
+    for(positional <- positionals) {
+      if(sargs.isEmpty) {
+        throw new IntermediateParsingException("not all positional arguments were provided")
       }
-      for (positional <- positionals) {
-        if (!positional.parsed) {
-          remaining = positional.parse(sargs)
-          if(remaining.length < sargs.length) {
-            remaining = this.parse(remaining)
-          }
-        }
+      sargs = positional.parse(sargs)
+    }
+    sargs
+  }
+
+  private def parseOptionals(args: List[String]): List[String] = {
+    var sargs = args
+    for(optional <- optionals) {
+      if(sargs.nonEmpty && !optional.parsed) {
+        sargs = optional.parse(sargs)
       }
-      assert(remaining.length == sargs.length || remaining.isEmpty,
-        s"either no positional is parsed or parsing is done but $args remain")
-      for (optional <- optionals) {
-        if (!optional.parsed) {
-          remaining = optional.parse(sargs)
-          if(remaining.length < sargs.length) {
-            remaining = this.parse(remaining)
-          }
-        }
-      }
-      assert(remaining.length == sargs.length || remaining.isEmpty,
-        s"either no optional is parsed or parsing is done but $args remain")
-      breakable {
+    }
+    sargs
+  }
+
+  private def parseSubparser(args: List[String]) : List[String] = {
+    breakable {
+      if(subparsers.nonEmpty) {
         for (subparser <- subparsers) {
-          if (subparser.name == sargs.head) {
-            remaining = subparser.parse(sargs)
+          if (args.isEmpty) {
+            throw new IntermediateParsingException("no more arguments and a subaction has still to be chosen")
+          }
+          if (subparser.name == args.head) {
+            val remain = subparser.parse(args)
+            if (remain.nonEmpty) {
+              throw new ParsingException(s"there is junk at the end of the arguments ${remain}")
+            }
             break()
           }
         }
+        throw new IntermediateParsingException("no subaction was chosen")
       }
     }
-    assert(remaining.isEmpty, s"after applying all parsers $remaining still remains")
-    assert(positionals.map(_.parsed).reduceOption(_ && _).getOrElse(true), "at least one positional was not satisfied")
-    assert(subparsers.map(_.parsed).reduceOption(_ || _).getOrElse(true), "no subparser was triggered")
     Nil
+  }
+
+  override def parse(args: List[String]): List[String] = {
+    try {
+      assert((args.nonEmpty && args.head == this.name) || parentParsers.isEmpty)
+      parsed = true
+      parseSubparser(parseOptionals(parsePositionals(if(parentParsers.isEmpty) args else args.tail)))
+    } catch {
+      case x : IntermediateParsingException =>
+        println(this.help())
+        throw new ParsingException(x.getMessage)
+      case _ : HelpException =>
+        println(this.help())
+        throw new ParsingException("The help flag was encountered")
+    }
   }
 
   override def help(): String = {
@@ -204,13 +226,17 @@ private case class Parser(override val name : String, override val description :
     msg ++= ","
     msg ++= optionals.filter(_.isInstanceOf[Flag])
       .map(x => s"${x.asInstanceOf[Flag].short}").mkString(",")
+    msg ++= ",-h,--help"
     msg ++= "} "
     msg ++= subparsers.map(_.name).mkString(" ")
     msg ++= "\n\n"
-    msg ++= description + "\n\n"
-    msg ++= (positionals ++ optionals).map(_.help()).mkString("\n")
-    msg ++= "\n"
-    msg ++= subparsers.map(sp => s"${sp.name} ${sp.description}").mkString("\n")
+    msg ++= description + "\n\n          "
+    msg ++= (positionals ++ optionals).map(_.help()).mkString("\n\n          ")
+    msg ++= "\n\n          "
+    msg ++= "-h/--help prints this help message"
+    msg ++= "\n\n          "
+    msg ++= subparsers.map(sp => s"${sp.name} ${sp.description}").mkString("\n\n          ")
+    msg ++= "\n\n\n"
     msg.toString
     }
 }
